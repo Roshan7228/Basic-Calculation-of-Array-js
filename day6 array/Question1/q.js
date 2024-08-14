b2180fe96c753fdc5e06e2a7118eb82395598090
Roshan kumar <168442823+Roshan7228@users.noreply.github.com>
2024-08-14 18:04:29 +0530

    Add files via upload

---
+++ day6 array/Question1/q.js
@@ -0,0 +1,9 @@
+// write a program to find the even number,
+let arr=[1,2,3,4,5,7,6,8,9,10];
+function even(value){
+    let even=value.filter((Element)=>{
+        return Element%2==0;
+    })
+    return even;
+}
+console.log(even(arr));
---
+++ day6 array/Question2/q.js
@@ -0,0 +1,9 @@
+// write a program to calculate a average given the array.
+let array=[1000,2000,3000,4000,5000];
+function average(value){
+    let total=value.reduce((prev,curra)=>{
+        return prev+curra;
+    })
+    return(total/array.length);
+}
+console.log(average(array));
